0598e8f76deab8ef0c72aa67394221e2a44c1df2
IMHOJEONG <39ghwjd@naver.com>
2019-08-25 03:41:06 +0900

    Add about

--- src/About.js
+++ src/About.js
@@ -1,23 +1,35 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, Button, Container, Col, Row } from 'react-bootstrap';
 import './About.css';
 
 class About extends Component{
     render(){
         return(
-            <>
-                <div class="WHAT-WE-DO">
-                    what we do 
-                </div>
-                <div class="We-connect-to-people">
-                We connect to people who want to fight a match to help people to get inspired <br />
+            <div>
+            <Container className="justify-content-between">
+                <Row xs lg="2"></Row>
+                <Row className="justify-content-md-center text-center">
+                    <div class="WHAT-WE-DO">
+                        what we do 
+                    </div>
+                </Row>
+                <Row className="justify-content-md-end"></Row>
+            </Container>
+            <Container className="justify-content-between">
+                <Row xs lg="2"></Row>
+                <Row className="justify-content-md-center text-center">
+                    <div class="We-connect-to-people">
+                    We connect to people who want to fight a match to help people to get inspired <br />
 workout more often. Through pose estimation technology, <br />
 we track your bone <br />
 structure and count your movements. <br /> 
-                </div>
-            </>
+                    </div>
+                </Row>
+                <Row className="justify-content-md-end"></Row>
+            </Container>
+            </div>
         );
     }
-
 }
-
 export default About;
--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, Button, Container, Col, Row } from 'react-bootstrap';
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Link  } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch  } from 'react-router-dom';
 import Main from './step1/main';
 import round from './round.png';
+import About from './About';
 // import { fire, getFireDB } from './Firebase';
 
 // 
@@ -12,6 +13,49 @@ import round from './round.png';
 // function ButtonStart(){
 //   return <Button>Start</Button>;
 // }
+
+
+function ContainerHeader() {
+  return <div>
+  <canvas id="balls"></canvas>
+  <Container className="justify-content-between">
+  <Row xs lg="2"></Row>
+  <Row className="justify-content-md-center text-center">
+      <div id="Rectangle">
+      PUMP OR DEATH - THE DEATH OF COMPETITION
+      </div>
+      
+  </Row>
+  <Row className="justify-content-md-end"><img src={round}
+  srcset="round@2x.png 2x,
+    round@3x.png 3x"
+  class="round"></img></Row>
+  </Container>
+  <Container className="justify-content-between">
+  <Row xs lg="2"></Row>
+  <Row className="justify-content-md-center text-center">
+      <div id="track">
+      We track your poses.<br />
+      Inspire yourself while working out with people.
+      </div>
+      
+  </Row>
+  <Row className="justify-content-md-end"></Row>
+</Container>
+
+<Container className="justify-content-between">
+  <Row xs lg="2"></Row>
+  <Row className="justify-content-md-center text-center">
+      <div id="box" style={{display: 'flex', 
+justifyContent: 'center', alignItems: 'center'}}>
+          WHATTT
+      </div>
+      
+  </Row>
+  <Row className="justify-content-md-end"></Row>
+</Container>
+</div> ;
+}
 
 export default class App extends Component{
 
@@ -31,140 +75,107 @@ export default class App extends Component{
   //       })
   //     });
   // }
-  componentDidMount(){
-    var canvas = document.getElementById('balls');
-    console.log(canvas);
-    var width = canvas.width = 1800;
-    var height = canvas.height = 1000;
+  // componentDidMount(){
+  //   var canvas = document.getElementById('balls');
+  //   // console.log(canvas);
+  //   var width = canvas.width = 1800;
+  //   var height = canvas.height = 1000;
     
-    function random(min,max){
-      return Math.floor(Math.random() * (max - min + 1))+min;
-    }
+  //   function random(min,max){
+  //     return Math.floor(Math.random() * (max - min + 1))+min;
+  //   }
     
-    if(canvas.getContext){
-      var theSurface = canvas.getContext('2d');
+  //   if(canvas.getContext){
+  //     var theSurface = canvas.getContext('2d');
       
-      function Ball(x,y,color,velX,velY,size){
-        this.x = x;
-        this.y = y;
-        this.velX = velX;
-        this.velY = velY;
-       this.color = color;
-        this.size = size;
-        this.draw = function(){
-          theSurface.beginPath();
-          theSurface.arc(this.x,this.y,50,12,Math.PI*2,true);
-          theSurface.fillStyle = this.color;
-          theSurface.fill();
-        }
-        this.update = function(){
-          if((this.x + this.size) >= width ){
-             this.velX = -(this.velX);
-             }
+  //     function Ball(x,y,color,velX,velY,size){
+  //       this.x = x;
+  //       this.y = y;
+  //       this.velX = velX;
+  //       this.velY = velY;
+  //      this.color = color;
+  //       this.size = size;
+  //       this.draw = function(){
+  //         theSurface.beginPath();
+  //         theSurface.arc(this.x,this.y,50,12,Math.PI*2,true);
+  //         theSurface.fillStyle = this.color;
+  //         theSurface.fill();
+  //       }
+  //       this.update = function(){
+  //         if((this.x + this.size) >= width ){
+  //            this.velX = -(this.velX);
+  //            }
           
-          if((this.x - this.size) <= 0 ){
-             this.velX = -(this.velX);
-             }
+  //         if((this.x - this.size) <= 0 ){
+  //            this.velX = -(this.velX);
+  //            }
           
-          if((this.y + this.size) >= height ){
-             this.velY = -(this.velY);
-             }
-          if((this.y - this.size) <= 0 ){
-             this.velY = -(this.velY);
-             }
+  //         if((this.y + this.size) >= height ){
+  //            this.velY = -(this.velY);
+  //            }
+  //         if((this.y - this.size) <= 0 ){
+  //            this.velY = -(this.velY);
+  //            }
           
-          this.x += this.velX;
-          this.y += this.velY;
-          // theSurface.bezierCurveTo();
-        }
+  //         this.x += this.velX;
+  //         this.y += this.velY;
+  //         // theSurface.bezierCurveTo();
+  //       }
         
         
-      }
-      var balls = [];
+  //     }
+  //     var balls = [];
       
-      function loop(){
-        theSurface.fillStyle = 'rgba(255, 255, 255, 0.02)';
-        theSurface.fillRect(0,0,width,height);
+  //     function loop(){
+  //       theSurface.fillStyle = 'rgba(255, 255, 255, 0.02)';
+  //       theSurface.fillRect(0,0,width,height);
         
-        while(balls.length < 20){
-        var myBall = new Ball(random(0,width),random(0,height),'rgb('+/*random(0,255)*/0+','+/*random(0,255)*/0+','+/*random(0,255)*/0+')',random(-7,7),random(-7,7),random(10,20));
-        balls.push(myBall);
-      }
+  //       while(balls.length < 20){
+  //       var myBall = new Ball(random(0,width),random(0,height),'rgb('+/*random(0,255)*/0+','+/*random(0,255)*/0+','+/*random(0,255)*/0+')',random(-7,7),random(-7,7),random(10,20));
+  //       balls.push(myBall);
+  //     }
         
-        for(var j = 0; j < balls.length;j++){
-          balls[j].draw();
-          balls[j].update();
-        }
+  //       for(var j = 0; j < balls.length;j++){
+  //         balls[j].draw();
+  //         balls[j].update();
+  //       }
         
-        requestAnimationFrame(loop); 
+  //       requestAnimationFrame(loop); 
         
-      }
-      loop();
+  //     }
+  //     loop();
       
-    }
-  }
+  //   }
+  // }
 
   render(){
     // const { memo } = this.state;
     return (
       <>
         <Router>
-        <Navbar bg="light" variant="light" className="justify-content-between">
-          <Navbar.Brand href="#home" className="FontExtra">PUMPETITION</Navbar.Brand>
-          <Navbar.Collapse className="justify-content-end FontExtra">
-            <Nav.Link href="#about" style={{color:'black'}}>ABOUT</Nav.Link>
-            <Link to="/step1/Main" id="Ready" style={{display: 'flex', 
-  justifyContent: 'center', alignItems: 'center'}}>Ready?</Link>
+          <Navbar bg="light" variant="light" className="justify-content-between">
+            <Navbar.Brand href="/" className="FontExtra">PUMPETITION</Navbar.Brand>
+            <Navbar.Collapse className="justify-content-end FontExtra">
+              <Link to="/About" style={{color:'black'}}>ABOUT</Link>
+              <Link to="/step1/Main" id="Ready" style={{display: 'flex', 
+    justifyContent: 'center', alignItems: 'center'}}>Ready?</Link>
 
-            {/* <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link> */}
-          </Navbar.Collapse>
-          {/* <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-primary">Search</Button>
-          </Form> */}
-        </Navbar>
-          {/* {memo.p2}  */}
-          <canvas id="balls"></canvas>
-          <Container className="justify-content-between">
-              <Row xs lg="2"></Row>
-              <Row className="justify-content-md-center text-center">
-                  <div id="Rectangle">
-                  PUMP OR DEATH - THE DEATH OF COMPETITION
-                  </div>
-                  
-              </Row>
-              <Row className="justify-content-md-end"><img src={round}
-               srcset="round@2x.png 2x,
-                round@3x.png 3x"
-              class="round"></img></Row>
-          </Container>
-          <Container className="justify-content-between">
-              <Row xs lg="2"></Row>
-              <Row className="justify-content-md-center text-center">
-                  <div id="track">
-                  We track your poses.<br />
-                  Inspire yourself while working out with people.
-                  </div>
-                  
-              </Row>
-              <Row className="justify-content-md-end"></Row>
-          </Container>
-
-          <Container className="justify-content-between">
-              <Row xs lg="2"></Row>
-              <Row className="justify-content-md-center text-center">
-                  <div id="box" style={{display: 'flex', 
-  justifyContent: 'center', alignItems: 'center'}}>
-                      WHATTT
-                  </div>
-                  
-              </Row>
-              <Row className="justify-content-md-end"></Row>
-          </Container>
-
-          <Route exact path="/step1/Main" component={Main} />
-          </Router>
+              {/* <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link> */}
+            </Navbar.Collapse>
+            {/* <Form inline>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-primary">Search</Button>
+            </Form> */}
+          </Navbar>
+            {/* {memo.p2}  */}
+              <Switch>
+                <Route exact path="/" component={ContainerHeader}/>
+                <Route path="/About" component={About}/>
+                <Route path="/step1/Main" component={Main} />
+              
+              </Switch> 
+           </Router>
       </>  
         
     );
